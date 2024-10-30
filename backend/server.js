f43ccb05a4052cd6ac92d4026ba83a5af80bca97
erenofc28 <rcdanteff@gmail.com>
2024-10-30 14:27:51 +0530

    jii

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,13 @@ app.use(cors());
 app.use(express.json()); // allow us to accespt json data on req.body
 dotenv.config();
 app.use("/products",router)
-if (process.env.NODE_ENV === "production"){
+// if (process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname,"/frontend/dist")) )
   app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
   })
 
-}
+// }
 const port = process.env.PORT || 5000
 //code nfJ9XdEjytiLCZKQ 
 app.listen(port,()=>{
